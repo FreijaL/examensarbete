781f9983657f035f26516a8bb80781569ff3c5b6
FreijaL <110614499+FreijaL@users.noreply.github.com>
2024-02-27 10:58:37 +0100

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+function HomePage() {
+
+    return (
+        <>
+            <Header />
+            <h1>Test från homepage</h1>
+        </>
+    )
+}
+
+export default HomePage;
